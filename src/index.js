16ee8678556ee7cbd2929d51f57fb7a9c939747f
anyasuerte1 <anyasuerte@gmail.com>
2024-09-23 21:06:12 +0400

    create human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const units = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ];
+    const teens = [
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (number === 0) {
+        return "zero";
+    }
+
+    let words = "";
+
+    if (number >= 100) {
+        words += units[Math.floor(number / 100)] + " hundred ";
+        if (number % 100 === 0) {
+          words = words.trim();
+        }
+        number = number % 100;
+    }
+
+    if (number >= 10 && number < 20) {
+        return words + teens[number - 10];
+    }
+
+    if (number >= 20 && number < 100) {
+        words += tens[Math.floor(number / 10)];
+        if (number % 10 !== 0) {
+          words += ' ';
+      }
+        number = number % 10;
+    }
+
+    words += units[number];
+
+    return words;
+};
